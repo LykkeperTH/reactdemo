7ddb554aca7ecba6a27ddf5fd1cd1d6d3925df33
KITTINAN <rekise1@hotmail.com>
2020-10-29 23:31:26 +0700

    notting

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -3,35 +3,45 @@ import { Link } from "react-router-dom";
 
 const Profile = ({ user }) => {
   return (
-    <div className="card-profile ">
-      <img
-        className="w-100 "
-        src="https://www.meecapital.co.th/wp-content/uploads/2019/06/img_avatar.png"
-        alt="Avatar"
-      />
-      <div class="container">
-        <div>
-          <b>Name</b> : {user.name}
-        </div>
-        <div>
-          <b>Email</b> : {user.email}
-        </div>
-        <div>
-          <b>Address</b> : {user.address.street} {user.address.suit}{" "}
-          {user.address.city} {user.address.zipcode}
-        </div>
-        <div>
-          <b>Phone</b> : {user.phone}
-        </div>
-        <div>
-          <b>Website</b> : {user.website}
-        </div>
-        <div>
-          <b>Company</b> : {user.company.name} {user.company.catchPhrase}{" "}
-          {user.company.bs}
+    <div>
+      <div className="d-flex content-center pd-8p">
+        <div className="card-profile ">
+          <img
+            className="w-100 "
+            src="https://www.emsateknik.com.tr/wp-content/uploads/2018/01/avatar-png-1.png"
+            alt="Avatar"
+          />
+          <div class="container">
+            <div>
+              <b>Name</b> : {user.name}
+            </div>
+            <div>
+              <b>Email</b> : {user.email}
+            </div>
+            <div>
+              <b>Address</b> : {user.address.street} {user.address.suit}{" "}
+              {user.address.city} {user.address.zipcode}
+            </div>
+            <div>
+              <b>Phone</b> : {user.phone}
+            </div>
+            <div>
+              <b>Website</b> : {user.website}
+            </div>
+            <div>
+              <b>Company</b> : {user.company.name} {user.company.catchPhrase}{" "}
+              {user.company.bs}
+            </div>
+          </div>
         </div>
       </div>
-      <Link to={`/user/${user.id}/albums`}>More albums...</Link>
+      <div className="d-flex content-center mg-8p">
+        <Link to={`/user/${user.id}/albums`}>
+          <button className="button">
+            <span>More...</span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
